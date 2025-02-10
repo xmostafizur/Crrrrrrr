412bd412b7cdcd76f12a1136d8230c02f05a6723
xmostafizur <mdrahiyan45845@gmail.com>
2025-02-10 14:53:30 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+async function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await response.json();
+    document.getElementById("loginMessage").innerText = data.message;
+}
